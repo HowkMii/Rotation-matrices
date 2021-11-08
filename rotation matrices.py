b80e70b349b24512e04e301d97c0754f8b4389af
howkmii <abdelhakim@univ-tiaret.dz>
2021-11-08 17:36:25 +0100

    make it a row vector

--- rotation matrices.py
+++ rotation matrices.py
@@ -13,3 +13,22 @@ y
 plot_vectors([x], axes=[4, 4], fname='transform_x.svg')	
 
 plot_vectors([x, y], axes=[4, 4], fname='transformx_and_y.svg')
+
+
+
+angle = 100 * (np.pi / 180) #convert degrees to radians
+
+Ro = np.array([[np.cos(angle), -np.sin(angle)],
+              [np.sin(angle), np.cos(angle)]])
+
+x2 = np.array([2, 2]).reshape(1, -1) # make it a row vector
+y2 = np.dot(x2, Ro)
+
+print('Rotation matrix')
+print(Ro)
+print('\nRotated vector')
+print(y2)
+
+print('\n x2 norm', np.linalg.norm(x2))
+print('\n y2 norm', np.linalg.norm(y2))
+print('\n Rotation matrix norm', np.linalg.norm(Ro))
